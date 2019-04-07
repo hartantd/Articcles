e0667478fd8b340dba0ce1e71546f68ec1e651b3
hartantd <hartantd@mcmaster.ca>
2019-04-06 22:44:44 -0400

    Fix Bootstrap Jquery 2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require bootstrap-sprockets
 //= require jquery
+//= require bootstrap-sprockets
 //= require activestorage
 //= require turbolinks
 //= require_tree .
