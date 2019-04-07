45d93520839d1c350d05fb04d1d643f763e13b98
hartantd <hartantd@mcmaster.ca>
2019-04-07 03:48:37 -0400

    Fix DropDown Menu For Heroku Deployment V20

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery_ujs
 //= require rails-ujs
 //= require jquery
 //= require bootstrap-sprockets
+//= require jquery_ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,10 +27,14 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+
+  # Generate digests for assets URLs.
+  config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
-
+  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  config.force_ssl = true #zzz: uncommented from rails tutorial
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
@@ -51,7 +55,7 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
+  config.log_level = :info
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
