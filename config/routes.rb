4d2ceb995a6d97575493ac761a98665fee0c98de
hartantd <hartantd@mcmaster.ca>
2019-04-09 13:35:00 -0400

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 	get 'welcome/home', to: 'welcome#home'
 	get 'welcome/about', to: 'welcome#about'
 	resources :articles
+	get 'signup', to: 'users#new'
+	resources :users, except: [:new]
 end
