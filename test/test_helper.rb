14b1ebdec76459adb62ea333ca127acbbe14d3d1
hartantd <hartantd@mcmaster.ca>
2019-04-19 16:27:04 -0400

    Fix tests and update navbar with categories

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,12 +3,10 @@ require_relative '../config/environment'
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
-
-  # Add more helper methods to be used by all tests here...
-end
-
-def sign_in_as(user,password)
-	post login_path, params:{session:{email:user.email, password:password}}
+	  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+	  fixtures :all
+	  def sign_in_as(user, password)
+	  	post login_path, params:{ session:{ email: user.email, password: password}}
+	  # Add more helper methods to be used by all tests here...
+	end
 end
